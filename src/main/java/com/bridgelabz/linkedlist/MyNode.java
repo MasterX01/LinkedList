5ce04811702f675b41ee3ee12d29a0f071d4b3b9
MasterX01 <aakashsaxena14@gmail.com>
2021-03-25 20:56:59 +0530

    [Akash] ADD. working UC-1 code

---
+++ src/main/java/com/bridgelabz/linkedlist/MyNode.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.linkedlist;
+
+public class MyNode<K> {
+    public K key;
+    public MyNode<K> next;
+
+    public MyNode(K key){
+        this.key = key;
+        this.next = null;
+    }
+
+    public K getKey() {
+        return key;
+    }
+    public void setKey(K key) {
+        this.key = key;
+    }
+    public MyNode<K> getNext() {
+        return next;
+    }
+    public void setNext(MyNode<K> next) {
+        this.next = next;
+    }
+
+}
