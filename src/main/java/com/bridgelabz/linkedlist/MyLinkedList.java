325abee47e3bdf0204cb35dff71576699ac5d8d2
MasterX01 <aakashsaxena14@gmail.com>
2021-03-25 20:35:00 +0530

    [Akash] FIRST COMMIT

---
+++ src/main/java/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.linkedlist;
+
+public class MyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Linked List Program");
+    }
+}
